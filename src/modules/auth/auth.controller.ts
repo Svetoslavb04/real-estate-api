192cf9fce04b0dee8186ae884f652a8aa4208ac4
Svetoslav Borislavov <svetoslavb1234@gmail.com>
2025-05-31 10:20:49 +0300

    wip: auth
    Signed-off-by: Svetoslav Borislavov <svetoslavb1234@gmail.com>

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+
+class LoginDto {
+  email: string;
+  password: string;
+}
+
+@ApiTags('auth')
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  @ApiOperation({ summary: 'User login' })
+  @ApiResponse({ status: 200, description: 'User successfully logged in' })
+  @ApiResponse({ status: 401, description: 'Invalid credentials' })
+  async login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto.email, loginDto.password);
+  }
+}

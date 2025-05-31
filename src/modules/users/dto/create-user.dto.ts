58e054ffcbd028111c49f2accfcbb3eb509d90c8
Svetoslav Borislavov <svetoslavb1234@gmail.com>
2025-05-31 10:22:01 +0300

    feat: add users controller
    Signed-off-by: Svetoslav Borislavov <svetoslavb1234@gmail.com>

---
+++ src/modules/users/dto/create-user.dto.ts
@@ -0,0 +1,45 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  Length,
+  IsPhoneNumber,
+  IsOptional,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateUserDto {
+  @ApiProperty({ example: 'John', description: 'User first name' })
+  @IsNotEmpty()
+  @Length(2, 50)
+  firstName: string;
+
+  @ApiProperty({ example: 'Doe', description: 'User last name' })
+  @IsNotEmpty()
+  @Length(2, 50)
+  lastName: string;
+
+  @ApiProperty({ example: 'john@example.com', description: 'User email' })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty({ example: 'password123', description: 'User password' })
+  @IsNotEmpty()
+  @Length(6, 100)
+  password: string;
+
+  @ApiProperty({ example: '+359888123456', description: 'User phone number' })
+  @IsPhoneNumber()
+  phoneNumber: string;
+
+  @ApiProperty({ example: 'agent', description: 'User role' })
+  @IsOptional()
+  role?: string;
+
+  @ApiProperty({
+    example: 'Experienced real estate agent',
+    description: 'User description',
+  })
+  @IsOptional()
+  description?: string;
+}
